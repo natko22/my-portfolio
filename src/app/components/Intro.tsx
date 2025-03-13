62d7aede1d527b9ee267a776f227acf170138665
Natassa <112552395+natko22@users.noreply.github.com>
2025-03-13 15:36:22 +0100

    bring back intro

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -3,14 +3,14 @@ export default function Intro() {
     <section className="intro flex flex-col justify-center items-center text-center px-6 md:px-12 space-y-4 md:space-y-2 mt-10 md:mt-10 h-[10vh]">
       {/* Title */}
       <h1 className="text-book-accent-dark text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
-        {/* Turning Pages, Writing Code */}
+        Turning Pages, Writing Code
         {/* Lines of Code, Chapters of Creativity */}
       </h1>
 
       <p className="w-full text-center max-w-2xl italic text-book-accent-dark text-lg sm:text-xl md:text-2xl mx-auto">
         {/* Code is my ink, design is my story—turn the page and explore. */}
         {/* Code is my ink, imagination is my framework—turn the page and explore. */}
-        {/* Where logic meets creativity, a new world unfolds */}
+        Where logic meets creativity, a new world unfolds
       </p>
     </section>
   );
