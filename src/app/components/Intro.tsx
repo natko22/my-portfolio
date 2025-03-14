09aedd4d91dd95e2a1a7eb060705fda6bd594abb
Natassa <112552395+natko22@users.noreply.github.com>
2025-03-14 09:46:17 +0100

    adjust intro's responsiveness

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,6 +1,6 @@
 export default function Intro() {
   return (
-    <section className="intro flex flex-col justify-center items-center text-center px-6 md:px-12 space-y-4 md:space-y-2 mt-10 md:mt-10 h-[10vh]">
+    <section className="intro flex flex-col justify-center items-center text-center px-6 md:px-12 space-y-4 md:space-y-2 mt-4 sm:mt-6 md:mt-10 lg:mt-10 pt-12 sm:pt-8 md:pt-4 lg:pt-0 h-auto min-h-[10vh]">
       {/* Title */}
       <h1 className="text-book-accent-dark text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
         Turning Pages, Writing Code
