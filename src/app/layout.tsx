79357480fc344c3ff9eaffa8acf2b5fa15a4882d
Natassa <112552395+natko22@users.noreply.github.com>
2025-04-09 16:32:43 +0200

    add correct favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   title: "Turning Pages, Writing Code | Portfolio",
   icons: {
     icon: [
-      { url: "/favicon1.svg", type: "image/svg+xml" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
       // { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       // { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       // { url: "/favicon.ico", type: "image/x-icon" },
