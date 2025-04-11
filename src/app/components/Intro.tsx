1df67fd6add6f5002f5a9bd29ae93153de43fa2c
Natassa <112552395+natko22@users.noreply.github.com>
2025-04-11 13:29:12 +0200

    update meta layout -change title

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -5,14 +5,12 @@ export default function Intro() {
       aria-label="Intro section"
     >
       {/* Title */}
-      <h1 className="text-book-accent-dark text-3xl sm:text-4xl md:text-5xl lg:text-6xl mt-8 whitespace-nowrap">
-        Turning Pages, Writing Code
-        {/* Lines of Code, Chapters of Creativity */}
+      <h1 className="text-book-accent-dark text-3xl sm:text-3xl md:text-4xl lg:text-5xl mt-8 whitespace-nowrap">
+        {/* Turning Pages, Writing Code */}
+        Lines of Code, Chapters of Creativity
       </h1>
 
       <p className="w-full text-center max-w-2xl italic text-book-accent-dark text-base sm:text-lg md:text-xl mx-auto mt-2">
-        {/* Code is my ink, design is my story—turn the page and explore. */}
-        {/* Code is my ink, imagination is my framework—turn the page and explore. */}
         Where logic meets creativity, a new world unfolds
       </p>
     </section>
