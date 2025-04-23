d354238ac9a2b3448c9d9a02cbdca6155c0e24c6
Natassa <112552395+natko22@users.noreply.github.com>
2025-04-23 14:59:32 +0200

    test height only for mobile

--- src/app/components/BookLayout.tsx
+++ src/app/components/BookLayout.tsx
@@ -90,9 +90,9 @@ const BookLayout: React.FC<BookLayoutProps> = memo(
     const bookHeightClass =
       isOpen && windowWidth !== null
         ? windowWidth < 375 // xxxs to xxs
-          ? "h-[85vh]" // Ultra small screens
+          ? "h-[80vh]" // Ultra small screens
           : windowWidth < 640 // xxs to sm
-          ? "h-[88vh]" // Small screens
+          ? "h-[83vh]" // Small screens
           : windowWidth < 768 // sm to md
           ? "h-[80vh]" // Medium-small screens
           : windowWidth < 900 // md to tablet
